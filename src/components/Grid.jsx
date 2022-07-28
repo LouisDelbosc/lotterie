cf949eb3d56dc4dd8db330684431374c9fd484f2
Louis Delbosc <louisdelbosc@gmail.com>
2022-07-28 13:57:27 +0200

    Made a small grid

---
+++ src/components/Grid.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export function Grid() {
+  const gridItems = [
+    [0, 1, 2, 3, 4, 5, 6, 7],
+    [8, 9, 10, 11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20, 21, 22, 23],
+    [24, 25, 26, 27, 28, 29, 30, 31],
+    [32, 33, 34, 35, 36, 37, 38, 39],
+    [40, 41, 42, 43, 44, 45, 46, 47],
+    [48, 49, 50, 51, 52, 53, 54, 55],
+    [56, 57, 58, 59, 60, 61, 62, 63],
+  ];
+  console.log(gridItems);
+  return (
+    <>
+      <div>wehs</div>
+      <table>
+        {gridItems.map((row) => (
+          <tbody>
+            <tr>
+              {row.map((item) => (
+                <td className="p-3">
+                  {" "}
+                  <GridItem data={item} />
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        ))}
+      </table>
+    </>
+  );
+}
+
+function GridItem({ data }) {
+  return <div>{data}</div>;
+}
